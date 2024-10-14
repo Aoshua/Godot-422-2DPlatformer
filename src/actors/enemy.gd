b074b8c7b38ad6d722ae738a5480f145864d3c33
Josh Abbott <39091681+Aoshua@users.noreply.github.com>
2024-10-14 15:59:24 -0600

    added enemies

---
+++ src/actors/enemy.gd
@@ -0,0 +1,16 @@
+extends Actor
+
+func _ready() -> void:
+	velocity.x = -speed.x
+
+func _on_stop_detector_body_entered(body: Node2D) -> void:
+	if body.global_position.y > %StompDetector.global_position.y:
+		return 
+	get_node("CollisionShape2D").disabled = true
+	queue_free()
+
+func _physics_process(delta: float) -> void:
+	velocity.y += gravity * delta
+	if is_on_wall():
+		velocity.x *= -1.0
+	move_and_slide()
