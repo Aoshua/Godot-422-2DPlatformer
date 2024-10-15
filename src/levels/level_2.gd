332258d3b1d544fbebf43dc3d550385332886364
Josh Abbott <39091681+Aoshua@users.noreply.github.com>
2024-10-15 09:12:56 -0600

    parity with demo proj

---
+++ src/levels/level_2.gd
@@ -0,0 +1,11 @@
+extends Node2D
+
+@onready var player = $Player 
+@onready var camera = player.get_node("Camera2D")
+
+func _ready():
+	set_camera_limits()
+
+func set_camera_limits():
+	camera.limit_left = -168
+	camera.limit_right = 7748
